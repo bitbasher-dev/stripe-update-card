3848c1525861519f29046bb53abd1afd5485c760
Pedro Palhari <pedropalhari@gmail.com>
2023-02-10 12:44:21 -0300

    added out

---
+++ www/out/_next/static/ByKxHeMfDEBWc6rcrIgnN/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/982-21581c7a3124e11c.js","static/chunks/pages/index-e670f47e3db3adc8.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
